80424ccb9731c54be77cc2770c6e49d547975173
Dmitry <dimontiranid1999@gmail.com>
2018-03-14 14:08:57 +0300

    generics

---
+++ ChatClient/GenericsWPF.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+using System.Xml;
+using System.IO;
+
+namespace ChatClient
+{
+	class GenericsWPF<T>
+	{
+		/// Generic Method to perform Deep-Copy of a WPF element (e.g. UIElement)
+		public static T DeepDarkCopy(T element)
+		{
+			var xaml = XamlWriter.Save(element);
+			var xamlString = new StringReader(xaml);
+			var xmlTextReader = new XmlTextReader(xamlString);
+			var deepCopyObject = (T)XamlReader.Load(xmlTextReader);
+			return deepCopyObject;
+		}
+	}
+}
--- ChatClient/MainWindow.xaml.cs
+++ ChatClient/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Markup;
+using System.Xml;
 
 namespace ChatClient
 {
@@ -24,6 +27,32 @@ namespace ChatClient
 		{
 			InitializeComponent();
 			
+		}
+		
+		/*
+		 <Grid Width="374" Height="80" Background="#FFFFE8E8">
+            <TextBlock HorizontalAlignment="Left" Margin="10,10,0,0" TextWrapping="Wrap" Text="Канал для успешных трейдеров" VerticalAlignment="Top" Height="32" Width="307" FontSize="14" FontFamily="Tahoma" FontWeight="Bold"/>
+            <TextBlock HorizontalAlignment="Left" Margin="322,10,0,0" TextWrapping="Wrap" Text="8" VerticalAlignment="Top" Height="32" Width="42" FontSize="20" TextAlignment="Center" FontFamily="Tahoma"/>
+            <TextBlock HorizontalAlignment="Left" Margin="10,42,0,0" TextWrapping="Wrap" Text="128 участников" VerticalAlignment="Top" Height="22" Width="163" FontFamily="Tahoma" FontSize="14"/>
+        </Grid>
+		 */
+		private Grid createChannelGrid(string name, int newM, int users)
+		{
+			var g1 = GenericsWPF<Grid>.DeepDarkCopy(sampleGrid);
+			g1.Visibility = Visibility.Visible;
+			var ch = g1.Children;
+			((TextBlock)ch[0]).Text = name;
+			((TextBlock)ch[1]).Text = ""+newM;
+			((TextBlock)ch[2]).Text = users + " участников";
+			return g1;
+		}
+
+
+		private int indx = 1;
+		private void Ellipse_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			channelList.Items.Add(createChannelGrid("канал " + indx, 2+indx, 5+indx));
+			indx++;
 		}
 	}
 }
