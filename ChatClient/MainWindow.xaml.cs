2726b2cbfae44ad4269e803cdb716ca2497eeb9d
Dmitry <dimontiranid1999@gmail.com>
2018-03-15 14:12:11 +0300

    added sending message ability

--- ChatClient/MainWindow.xaml.cs
+++ ChatClient/MainWindow.xaml.cs
@@ -41,18 +41,49 @@ namespace ChatClient
 			var g1 = GenericsWPF<Grid>.DeepDarkCopy(SampleGrid);
 			g1.Visibility = Visibility.Visible;
 			var ch = g1.Children;
-			((TextBlock)ch[0]).Text = name;
-			((TextBlock)ch[1]).Text = ""+newM;
-			((TextBlock)ch[2]).Text = users + " участников";
+			((TextBlock) ch[0]).Text = name;
+			((TextBlock) ch[1]).Text = ""+newM;
+			((TextBlock) ch[2]).Text = users + " участников";
 			return g1;
 		}
-
 
 		private int indx = 1;
 		private void Ellipse_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
 		{
 			ChannelList.Items.Add(createChannelGrid("канал " + indx, 2+indx, 5+indx));
 			indx++;
+		}
+
+
+		/*
+		 <Grid Name="MessageGrid" Width="374" VerticalAlignment="Top" Background="#FFBEE7E8" Visibility="Collapsed">
+            <Grid.RowDefinitions>
+                <RowDefinition Height="32"></RowDefinition>
+                <RowDefinition Height="*"></RowDefinition>
+            </Grid.RowDefinitions>
+            <TextBlock Grid.Row="0" HorizontalAlignment="Left" Margin="10,10,0,0" TextWrapping="Wrap" Text="Username" VerticalAlignment="Top" Height="32" Width="307" FontSize="14" FontFamily="Tahoma" FontWeight="Bold"/>
+            <TextBlock Grid.Row="1" HorizontalAlignment="Left" Margin="10,0,0,0" TextWrapping="Wrap" Text="some message dddddddddddddddddddddddddddddddddddddddddddddddddd" VerticalAlignment="Top"  Width="300" FontFamily="Tahoma" FontSize="14"/>
+        </Grid>
+		 */
+		private Grid createMessageGrid(string name, string message)
+		{
+			var g1 = GenericsWPF<Grid>.DeepDarkCopy(MessageGrid);
+			g1.Visibility = Visibility.Visible;
+			var ch = g1.Children;
+			((TextBlock) ch[0]).Text = name;
+			((TextBlock) ch[1]).Text = message;
+			return g1;
+		}
+
+		
+		private void TextBox_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Return)
+			{
+				string username = "me";
+				MessageList.Items.Add(createMessageGrid(username, MessageTextBox.Text));
+				MessageTextBox.Text = "";
+			}
 		}
 	}
 }
